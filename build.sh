a1a4f5d26df3102d336db6e9229591b1b9d8ba6b
Satoshi Nagayasu <snaga@uptime.jp>
2017-11-26 10:07:32 +0900

    WIP.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-docker rmi postgres4analytics
-docker build -t postgres4analytics .
+#docker build -v /tmp/docker-build:/tmp/docker-build -f Dockerfile.postgres -t postgres .
+#docker build -v /tmp/docker-build:/tmp/docker-build -f Dockerfile.madlib-build -t madlib-build .
+docker rmi madlib
+docker build -v /tmp/docker-build:/tmp/docker-build -f Dockerfile.madlib -t madlib .
 docker images
-
